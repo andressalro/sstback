e01a8d8e47638e9e865ff81561ddc9bc10c30e23
Andrés Salgado Rojas <andressalgadorojas@gmail.com>
2023-09-23 03:39:59 -0500

    Configurar el contenedor de dependencias

--- src/config/container.config.ts
+++ src/config/container.config.ts
@@ -1,1 +1,9 @@
+import "reflect-metadata"; // Importante: requerido por Tsyringe
+import { container } from "tsyringe";
+import { PostValidateAnomaly } from "../application/anomaly/usecases/postValidateAnomaly";
+import { GetAnomalyStatistics } from "../application/anomaly/usecases/getAnomalyStatistics";
 
+container.register("PostValidateAnomaly", { useClass: PostValidateAnomaly });
+container.register("GetAnomalyStatistics", { useClass: GetAnomalyStatistics });
+
+export { container };
